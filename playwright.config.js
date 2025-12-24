b66707b744dc9d8237b3dbb0e150216c137378b7
DajaT <dtrotter03@gmail.com>
2025-12-23 20:52:29 -0500

    Updating github yaml file

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -49,6 +51,13 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+
+    {
+      name: 'edge',
+      use: {
+        ...devices['Desktop Edge'],
+        channel: 'msedge', // runs real Microsoft Edge
+      },
 
     /* Test against mobile viewports. */
     // {
@@ -69,7 +78,7 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+}],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
